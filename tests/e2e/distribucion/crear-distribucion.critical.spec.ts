34954db077d7affb655e8430ba9d0374c5451a5a
Aldis Joan <aabreu@soaint.com>
2026-03-20 12:01:54 -0400

    add:test

--- tests/e2e/distribucion/crear-distribucion.critical.spec.ts
+++ tests/e2e/distribucion/crear-distribucion.critical.spec.ts
@@ -4,7 +4,7 @@ test.describe('@distribucion @critical @E40-DIS-01', () => {
   test('@critical @distribucion @E40-DIS-01 debe mostrar acci\u00f3n Nueva Distribuci\u00f3n disponible', async ({
     distribucionPage,
   }) => {
-    await test.step('Validar presencia de acci\u00f3n', async () => {
+    await test.step('Validar la presencia de acci\u00f3n', async () => {
       await distribucionPage.expectNuevaDistribucionDisponible();
     });
   });
