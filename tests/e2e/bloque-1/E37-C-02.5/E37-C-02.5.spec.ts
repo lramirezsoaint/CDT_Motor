90f5385a78c08daa1189fcb29baf19014ea4d8b9
maidelis <mreyes@soaint.com>
2026-07-01 23:12:05 -0400

    Mapeo de tag con descripcion de CP

--- tests/e2e/bloque-1/E37-C-02.5/E37-C-02.5.spec.ts
+++ tests/e2e/bloque-1/E37-C-02.5/E37-C-02.5.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@fixtures/base.fixture';
 import { executeNegativeDeleteBlocked } from '../_shared/comunes-status-action';
 
 test.describe('@bloque1 @E37-C-02.5 ', () => {
- test('@bloque1 @E37-C-02.5 @procesos debe bloquear la eliminación de Maestro UoA cuando existe un proceso en ejecución', async ({
+ test('@bloque1 @E37-C-02.5 @eliminar debe bloquear la eliminación de Maestro UoA cuando existe un proceso en ejecución', async ({
  page,
  }) => {
  await executeNegativeDeleteBlocked(page, {
